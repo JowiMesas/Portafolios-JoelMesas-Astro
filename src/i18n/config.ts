029d5cab9b95cd664c71e7b87661f01af1f6749f
JowiMesas <joelmesash@gmail.com>
2025-03-14 13:56:58 +0100

    traductions

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,17 +1,17 @@
-import i18next from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import enTranslations from "../locales/en/translation.json";
-import esTranslations from "../locales/es/translation.json";
+// import i18next from "i18next";
+// import LanguageDetector from "i18next-browser-languagedetector";
+// import enTranslations from "../locales/en/translation.json";
+// import esTranslations from "../locales/es/translation.json";
 
-i18next.use(LanguageDetector).init({
-  fallbackLng: "es", // Idioma predeterminado (puedes cambiarlo a "en" si prefieres)
-  resources: {
-    en: {
-      translation: enTranslations,
-    },
-    es: {
-      translation: esTranslations,
-    },
-  },
-});
-export default i18next;
+// i18next.use(LanguageDetector).init({
+//   fallbackLng: "es", // Idioma predeterminado (puedes cambiarlo a "en" si prefieres)
+//   resources: {
+//     en: {
+//       translation: enTranslations,
+//     },
+//     es: {
+//       translation: esTranslations,
+//     },
+//   },
+// });
+// export default i18next;
