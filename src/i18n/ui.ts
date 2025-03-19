d54bfaef22ad34e94ffba31dcf51b79cb951580b
JowiMesas <joelmesash@gmail.com>
2025-03-19 13:58:22 +0100

    traductions fixed and running

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,6 +1,3 @@
-import enTranslation from "../../public/locales/en/translation.json";
-import esTranslation from "../../public/locales/es/translation.json";
-
 export const languages = {
   es: "Español",
   en: "English",
@@ -8,29 +5,120 @@ export const languages = {
 export const defaultLang = "es";
 export const ui = {
   es: {
-    'site.title': 'Portafolios de Joel Mesas',
-    'header.nav.home': 'Inicio',
-    'header.nav.about': 'Sobre Mi',
-    'header.nav.projects': 'Proyectos',
-    'header.nav.curriculum': 'Curriculum',
+    "site.title": "Portafolios de Joel Mesas",
+    "header.nav.home": "Inicio",
+    "header.nav.about": "Sobre Mi",
+    "header.nav.projects": "Proyectos",
+    "header.nav.curriculum": "Curriculum",
     //Footer
-    'footer.contacts':'Contactos',
-    'footer.phone': 'Teléfono',
-    'footer.view_profile': 'Ver Perfil',
-    'footer.web_developer': 'Desarrolador Web',
-
-      },
+    "footer.contacts": "Contactos",
+    "footer.phone": "Teléfono",
+    "footer.view_profile": "Ver Perfil",
+    "developerWeb": "Desarrollador Web",
+    //Page Home
+    "home.welcome": "¡Hola, soy",
+    "home.me": "Conóceme más",
+    "home.projects": "Ver Proyectos",
+    "home.valueCompany": "Lo que puedo aportar",
+    "home.contribution1": "Desarrollo a medida",
+    "home.contribution1.description": `Soluciones personalizadas que se adaptan a las necesidades
+     específicas de cada proyecto y cliente.`,
+    "home.contribution2": "Diseño centrado en usuario",
+    "home.contribution2.description": `Interfaces intuitivas y atractivas que priorizan
+     la experiencia del usuario final y mejoran la conversión.`,
+     "home.contribution3": "Adaptabilidad a nuevas tecnologías",
+     "home.contribution3.description": `Siempre me mantengo actualizado,aprendiendo y
+      adaptando nuevas tecnologías para optimizar y potenciar cada proyecto.`,
+    //Page Curriculum
+    "curriculum.title": "Sobre mi Currículum",
+    "curriculum.intro1": `Bienvenido a esta sección, donde podrás descargar mi currículum completo y 
+    explorar apartados específicos que destacan mis competencias clave. 
+    Además de mis habilidades interpersonales y competencias digitales, 
+    encontrarás un apartado dedicado a los idiomas.`,
+    "curriculum.intro2": `En este espacio destaco que, si bien mi lengua materna es el español y catalán,
+     el inglés es un área en la que estoy en constante mejora.
+      Mi compromiso por perfeccionar mis habilidades comunicativas me impulsa a 
+      seguir aprendiendo y adaptándome a contextos internacionales.`,
+    "curriculum.downloadCV": "Ver/Descargar Curriculum",
+    "curriculum.title2": "Apartados que te podrían interesar",
+    "curriculum.button.previous": "Anterior",
+    "curriculum.button.next": "Siguiente",
+    "curriculum.softSkills": "Habilidades",
+    "curriculum.technologies": "Tecnologías",
+    "curriculum.languages": "Idiomas",
+    "curriculum.info": `Estas son algunas de mis habilidades y competencias principales.
+     Para conocer más sobre mi trayectoria profesional, no dudes en descargar mi
+      currículum completo o explorar el resto de mi portafolio.`,
+    //Curriculum Soft Skills
+    "softSkills.teamwork": "Trabajo en Equipo",
+    "softSkills.organisation": "Organización",
+    "softSkills.responsibility": "Responsabilidad",
+    "softSkills.creativity": "Creatividad",
+    "softSkills.flexibility": "Flexibilidad",
+    "softSkills.empathy": "Empatía",
+    "softSkills.troubleshooting": "Resolución de Problemas",
+    //Curriculum Languages
+    "languages.spanish": "Español - Nativo",
+    "languages.catalan": "Catalán - Nativo",
+    "languages.english": "Inglés - En progreso",
+  },
   en: {
-    'site.title': 'Portfolio of Joel Mesas',
-    'header.nav.home': 'Home',
-    'header.nav.about': 'About Me',
-    'header.nav.projects': 'Projects',
-    'header.nav.curriculum' : 'Resume',
-    
+    "site.title": "Portfolio of Joel Mesas",
+    "header.nav.home": "Home",
+    "header.nav.about": "About Me",
+    "header.nav.projects": "Projects",
+    "header.nav.curriculum": "Resume",
+
     //Footer
-    'footer.contacts': 'Contacts',
-    'footer.phone': 'Phone',
-    'footer.view_profile': 'View Profile',
-    'footer.web_developer': 'Web Developer',
-     },
+    "footer.contacts": "Contacts",
+    "footer.phone": "Phone",
+    "footer.view_profile": "View Profile",
+    "developerWeb": "Web Developer",
+    //Page Home
+    "home.welcome": "Hello, I'm",
+    "home.me": "Get to know me ",
+    "home.projects": "See Projects",
+    "home.valueCompany": "What I can contribute",
+    "home.contribution1": "Tailor-made development",
+    "home.contribution1.description":
+      "Customised solutions that adapt to the specific needs of each project and client.",
+    "home.contribution2": "User-centred design",
+    "home.contribution2.description": `Intuitive and attractive interfaces that prioritise
+     the end-user experience and improve conversion.`,
+    "home.contribution3": "Adaptability to new technologies",
+    "home.contribution3.description": `I always keep myself updated, learning and
+     adapting new technologies to optimise and enhance each project.`,
+    //Page Curriculum
+    "curriculum.title": "About My CV",
+    "curriculum.intro1": `Welcome to this section, where you can download my 
+    full CV and explore specific sections highlighting my key competences. 
+    In addition to my interpersonal skills and digital competences,
+     you will find a section dedicated to languages.`,
+    "curriculum.intro2": `In this space I would like to emphasise that, although
+      my mother tongue is Spanish and Catalan, English is an area in 
+      which I am constantly improving. My commitment to perfecting my 
+      communication skills drives me to continue learning and adapting to international contexts.`,
+    "curriculum.downloadCV": "View/Download CV",
+    "curriculum.title2": "Sections you might be interested in",
+    "curriculum.button.previous": "Previous",
+    "curriculum.button.next": "Next",
+    "curriculum.softSkills": "Soft Skills",
+    "curriculum.technologies": "Technologies",
+    "curriculum.languages": "Languages",
+    "curriculum.info": `These are some of my core skills and competencies.
+     To learn more about my professional career, feel free to download my
+      full CV or explore the rest of my portfolio.`,
+    //Curriculum Soft Skills
+    "softSkills.teamwork": "Teamwork",
+    "softSkills.organisation": "Organisation",
+    "softSkills.responsibility": "Responsibility",
+    "softSkills.creativity": "Creativity",
+    "softSkills.flexibility": "Flexibility",
+    "softSkills.empathy": "Empathy",
+    "softSkills.troubleshooting": "Troubleshooting",
+    //Curriculum Languages
+    "languages.spanish": "Spanish - Native",
+    "languages.catalan": "Catalan - Native",
+    "languages.english": "English - In progress",
+  },
 } as const;
