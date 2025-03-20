81e79a35007d712276073f3173fc6817f7cb5987
JowiMesas <joelmesash@gmail.com>
2025-03-20 12:13:40 +0100

    traductions finished

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -2,6 +2,7 @@ export const languages = {
   es: "Español",
   en: "English",
 };
+import esTranslation from '../../public/locales/es/translation.json';
 export const defaultLang = "es";
 export const ui = {
   es: {
@@ -27,8 +28,61 @@ export const ui = {
     "home.contribution2.description": `Interfaces intuitivas y atractivas que priorizan
      la experiencia del usuario final y mejoran la conversión.`,
      "home.contribution3": "Adaptabilidad a nuevas tecnologías",
-     "home.contribution3.description": `Siempre me mantengo actualizado,aprendiendo y
-      adaptando nuevas tecnologías para optimizar y potenciar cada proyecto.`,
+     "home.contribution3.description": `Integración de nuevas tecnologías
+      que impulsa el desarrollo y fomenta la innovación en cada proyecto.`,
+     "home.contribution4": "Colaboración efectiva",
+     "home.contribution4.description": `Trabajo en equipo para lograr los objetivos del
+       proyecto en tiempo y forma.`,
+     "home.curriculum":"Ver mi curriculum",
+     "home.title2": "¿Listo para trabajar juntos?",
+     "home.title2.description": `Si buscas un desarrollador comprometido que
+      pueda llevar tus ideas al siguiente nivel, estoy aquí para ayudarte.`,
+      //Page AboutMe
+      "aboutMe.description": `Apasionado por crear soluciones innovadoras utilizando 
+      tecnologías como React, Laravel, Angular, Symfony y Astro.`,
+      "aboutMe.title": "Quién Soy",
+      "aboutMe.paragraph1": `Soy un desarrollador web con pasión por crear experiencias digitales
+       atractivas y funcionales. Disfruto trabajando en proyectos desafiantes que me permitan
+        aprender y crecer profesionalmente.`,
+     "aboutMe.paragraph2": `Me especializo en el desarrollo full stack, combinando habilidades
+      de frontend y backend para construir aplicaciones web completas. Siempre estoy buscando mejorar
+       mis habilidades y mantenerme al día con las últimas tendencias y tecnologías.`,
+     "aboutMe.paragraph3": `Fuera del mundo del código, me apasiona el mundo 
+     del motor 🏎️, la tecnología en general y los videojuegos 🎮.`,
+     "aboutMe.experience": "Mi experiencia",
+     "aboutMe.currently": "Actualmente",
+     "aboutMe.experience1": `Desarrollo de aplicaciones web con Laravel, optimizando 
+     interfaces y rendimiento. Colaboración en proyectos de clientes, implementando 
+     funcionalidades y asegurando la calidad del código.`,
+     "aboutMe.title.experience2": "Técnico en Sistemas Informáticos",
+     "aboutMe.experience2": `Instalación y configuración de software y hardware,
+      mantenimiento de equipos informáticos, resolución de incidencias técnicas
+       y documentación de procesos. Soporte técnico a profesores y estudiantes.`,
+      "aboutMe.eduaction": "Mi Formación",
+      "aboutMe.eduaction1": `Grado Superior en Desarrollo de Aplicaciones Web (DAW)`,
+      "aboutMe.education2": `Grado Medio en Sistemas Microinformáticos y Redes (SMX)`,
+
+      //Projects Page
+      "projects.my": "Mis",
+      "projects.title": "Proyectos",
+      "projects.title.description": `Aquí encontrarás una selección de los proyectos más
+       relevantes en los que he trabajado, mostrando mis habilidades técnicas y creatividad.`,
+      "projects.seeRepository": "Ver Repositorio",
+      "project1.description": `Este proyecto trata sobre una app de citas, 
+      desarrollada junto a 3 compañeros más en el grado superior de DAW.`,
+      "project2.description": `Este proyecto es sobre un blog en Laravel,
+       utilizando Livewire y Alpine.js, además de relaciones polimórficas para gestionar el contenido.`,
+      "project3.description": `Esta página web trata sobre videojuegos, en la que cuenta con diversas funciones:`,
+      "project3.function1": `Información sobre eventos de gaming`,
+      "project3.function2": `Noticias recientes con filtrado por categoría`,
+      "project3.function3": ` Foro para comunicación entre usuarios`,
+      "project4.title": "Carrera de Vehículos",
+      "project4.description": `Este proyecto fue creado en el grado superior de DAW, 
+      enfocado en el trabajo con clases de JavaScript. Principales funciones:`,
+      "project4.function1": `Crear y mostrar estadísticas de vehículos`,
+      "project4.function2": `Gestión de participantes y asignación de vehículos`,
+      "project4.function3": ` Creación y gestión de circuitos`,
+      "project4.function4": ` Simulación de carreras`,
     //Page Curriculum
     "curriculum.title": "Sobre mi Currículum",
     "curriculum.intro1": `Bienvenido a esta sección, donde podrás descargar mi currículum completo y 
@@ -86,8 +140,59 @@ export const ui = {
     "home.contribution2.description": `Intuitive and attractive interfaces that prioritise
      the end-user experience and improve conversion.`,
     "home.contribution3": "Adaptability to new technologies",
-    "home.contribution3.description": `I always keep myself updated, learning and
-     adapting new technologies to optimise and enhance each project.`,
+    "home.contribution3.description": `Integration of new technologies that drives development
+     and fosters innovation in each project.`,
+    "home.contribution4": "Effective collaboration",
+    "home.contribution4.description": `Teamwork to achieve project objectives in a timely manner.`,
+    "home.curriculum":"See my CV",
+    "home.title2": "Ready to work together?",
+    "home.title2.description": `If you are looking for a committed developer
+     who can take your ideas to the next level, I am here to help.`,
+     //Page AboutMe
+    "aboutMe.description": `Passionate about creating innovative solutions
+     using technologies such as React, Laravel, Angular, Symfony and Astro.`,
+     "aboutMe.title": "Who I am",
+     "aboutMe.paragraph1": `I am a web developer with a passion for creating engaging and
+      functional digital experiences. I enjoy working on challenging 
+      projects that allow me to learn and grow professionally.`,
+     "aboutMe.paragraph2": `I specialise in full stack development, combining frontend and 
+     backend skills to build complete web applications.I am always looking to improve my skills
+      and keep up to date with the latest trends and technologies.`,
+     "aboutMe.paragraph3": `Outside the world of coding, I am passionate about 
+     the world of the engine 🏎️, technology in general and videogames 🎮.`,
+     "aboutMe.experience": "My experience",
+     "aboutMe.currently": "Currently",
+     "aboutMe.experience1": `Development of web applications with Laravel, optimising interfaces and 
+     performance. Collaboration in client projects, implementing functionalities and ensuring code quality.`,
+     "aboutMe.title.experience2": "Computer Systems Technician",
+     "aboutMe.experience2": `Installation and configuration of software and hardware, 
+     maintenance of computer equipment, resolution of technical incidents and documentation
+      of processes. Technical support for teachers and students.`,
+      "aboutMe.eduaction": "My Education",
+      "aboutMe.eduaction1": `Higher Degree in Web Application Development (DAW)`,
+      "aboutMe.education2": `Intermediate Degree in Microcomputer Systems and Networks (SMX)`,
+
+          //Projects Page
+     "projects.my": "My",
+     "projects.title": "Projects",
+     "projects.title.description": `Here you will find a selection of the most 
+     relevant projects I have worked on, showcasing my technical skills and creativity.`,
+     "projects.seeRepository": "View Repository",
+     "project1.description": `This project is about a dating app, 
+     developed together with 3 other students in the DAW degree.`,
+     "project2.description": `This project is about a blog in Laravel, 
+     using Livewire and Alpine.js, plus polymorphic relations to manage the content.`,
+     "project3.description": `This website is about video games and has several functions:`,
+     "project3.function1": `Information on gaming events`,
+     "project3.function2": `Recent news with filtering by category`,
+     "project3.function3": `Forum for communication between users`,
+     "project4.title": "Vehicle Racing",
+     "project4.description": `This project was created in the higher DAW grade, 
+     focusing on working with JavaScript classes. Main functions:`,
+     "project4.function1": `Create and display vehicle statistics`,
+     "project4.function2": `Participant management and vehicle allocation`,
+     "project4.function3": `Creation and management of circuits`,
+     "project4.function4": `Racing simulation`,
     //Page Curriculum
     "curriculum.title": "About My CV",
     "curriculum.intro1": `Welcome to this section, where you can download my 
