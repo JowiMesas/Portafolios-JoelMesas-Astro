cf10e3900897f43d26db667c7dcdccd21d93f45f
Jowi <157366219+JowiMesas@users.noreply.github.com>
2025-03-24 16:00:29 +0100

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,16 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
-
+import tailwind from "@astrojs/tailwind";
 import tailwindcss from "@tailwindcss/vite";
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 export default defineConfig({
-  
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [
-    react(),
-  ],
+  integrations: [react(), tailwind()],
   i18n: {
-    defaultLocale: 'es',  // Your default language
-    locales: ['es', 'en'],
-    
-  }
+    defaultLocale: "es", // Your default language
+    locales: ["es", "en"],
+  },
 });
